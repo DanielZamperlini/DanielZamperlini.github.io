c8687bc350301d9d63974775a7def32b68c3af8d
DanielZamperlini <monkeydluffysan2021@gmail.com>
2022-02-21 18:04:54 -0300

    bug do menu

--- script/script.js
+++ script/script.js
@@ -1,130 +1,134 @@
-document
-  .querySelector(".navbar__toggle")
-  .addEventListener("click", function () {
+function navbarMenu() {
+  document
+    .querySelector(".navbar__toggle")
+    .addEventListener("click", function () {
+      this.classList.toggle("open");
+
+      document.querySelector(".menu").classList.toggle("open");
+    });
+  document.querySelector(".menu").addEventListener("click", function () {
     this.classList.toggle("open");
 
-    document.querySelector(".menu").classList.toggle("open");
+    document.querySelector(".navbar__toggle").classList.toggle("open");
   });
-document.querySelector(".menu").addEventListener("click", function () {
-  this.classList.toggle("open");
+}
+navbarMenu();
 
-  document.querySelector(".navbar__toggle").classList.toggle("open");
-});
+function cursosCompletos() {
+  const cursosFeitos = document.querySelectorAll(".faculdade h3");
+  cursosFeitos.forEach((item) => {
+    item.classList.add("item-cursos");
+  });
 
-const cursosFeitos = document.querySelectorAll(".faculdade h3");
-cursosFeitos.forEach((item) => {
-  item.classList.add("item-cursos");
-});
+  cursosFeitos.forEach((items) => {
+    const gradeCursos = document.querySelector(".etq1");
+    gradeCursos.addEventListener("mouseover", function () {
+      const items = document.querySelectorAll(".etq1 h3");
+      items.forEach((item) => {
+        item.classList.remove("item-cursos");
+        item.classList.add("item-cursosx");
+      });
+    });
+    gradeCursos.addEventListener("mouseout", function () {
+      const cursosFeitos = document.querySelectorAll(".etq1 h3");
+      cursosFeitos.forEach((item) => {
+        item.classList.add("item-cursos");
+        item.classList.remove("item-cursosx");
+      });
+    });
+  });
 
-cursosFeitos.forEach((items) => {
-  const gradeCursos = document.querySelector(".etq1");
-  gradeCursos.addEventListener("mouseover", function () {
-    const items = document.querySelectorAll(".etq1 h3");
-    items.forEach((item) => {
-      item.classList.remove("item-cursos");
-      item.classList.add("item-cursosx");
+  cursosFeitos.forEach((items) => {
+    const gradeCursos = document.querySelector(".etq2");
+    gradeCursos.addEventListener("mouseover", function () {
+      const items = document.querySelectorAll(".etq2 h3");
+      items.forEach((item) => {
+        item.classList.remove("item-cursos");
+        item.classList.add("item-cursosx");
+      });
+    });
+    gradeCursos.addEventListener("mouseout", function () {
+      const cursosFeitos = document.querySelectorAll(".etq2 h3");
+      cursosFeitos.forEach((item) => {
+        item.classList.add("item-cursos");
+        item.classList.remove("item-cursosx");
+      });
     });
   });
-  gradeCursos.addEventListener("mouseout", function () {
-    const cursosFeitos = document.querySelectorAll(".etq1 h3");
-    cursosFeitos.forEach((item) => {
-      item.classList.add("item-cursos");
-      item.classList.remove("item-cursosx");
-    });
-  });
-});
 
-cursosFeitos.forEach((items) => {
-  const gradeCursos = document.querySelector(".etq2");
-  gradeCursos.addEventListener("mouseover", function () {
-    const items = document.querySelectorAll(".etq2 h3");
-    items.forEach((item) => {
-      item.classList.remove("item-cursos");
-      item.classList.add("item-cursosx");
+  cursosFeitos.forEach((items) => {
+    const gradeCursos = document.querySelector(".etq3");
+    gradeCursos.addEventListener("mouseover", function () {
+      const items = document.querySelectorAll(".etq3 h3");
+      items.forEach((item) => {
+        item.classList.remove("item-cursos");
+        item.classList.add("item-cursosx");
+      });
+    });
+    gradeCursos.addEventListener("mouseout", function () {
+      const cursosFeitos = document.querySelectorAll(".etq3 h3");
+      cursosFeitos.forEach((item) => {
+        item.classList.add("item-cursos");
+        item.classList.remove("item-cursosx");
+      });
     });
   });
-  gradeCursos.addEventListener("mouseout", function () {
-    const cursosFeitos = document.querySelectorAll(".etq2 h3");
-    cursosFeitos.forEach((item) => {
-      item.classList.add("item-cursos");
-      item.classList.remove("item-cursosx");
-    });
-  });
-});
 
-cursosFeitos.forEach((items) => {
-  const gradeCursos = document.querySelector(".etq3");
-  gradeCursos.addEventListener("mouseover", function () {
-    const items = document.querySelectorAll(".etq3 h3");
-    items.forEach((item) => {
-      item.classList.remove("item-cursos");
-      item.classList.add("item-cursosx");
+  cursosFeitos.forEach((items) => {
+    const gradeCursos = document.querySelector(".etq4");
+    gradeCursos.addEventListener("mouseover", function () {
+      const items = document.querySelectorAll(".etq4 h3");
+      items.forEach((item) => {
+        item.classList.remove("item-cursos");
+        item.classList.add("item-cursosx");
+      });
+    });
+    gradeCursos.addEventListener("mouseout", function () {
+      const cursosFeitos = document.querySelectorAll(".etq4 h3");
+      cursosFeitos.forEach((item) => {
+        item.classList.add("item-cursos");
+        item.classList.remove("item-cursosx");
+      });
     });
   });
-  gradeCursos.addEventListener("mouseout", function () {
-    const cursosFeitos = document.querySelectorAll(".etq3 h3");
-    cursosFeitos.forEach((item) => {
-      item.classList.add("item-cursos");
-      item.classList.remove("item-cursosx");
-    });
-  });
-});
 
-cursosFeitos.forEach((items) => {
-  const gradeCursos = document.querySelector(".etq4");
-  gradeCursos.addEventListener("mouseover", function () {
-    const items = document.querySelectorAll(".etq4 h3");
-    items.forEach((item) => {
-      item.classList.remove("item-cursos");
-      item.classList.add("item-cursosx");
+  cursosFeitos.forEach((items) => {
+    const gradeCursos = document.querySelector(".etq5");
+    gradeCursos.addEventListener("mouseover", function () {
+      const items = document.querySelectorAll(".etq5 h3");
+      items.forEach((item) => {
+        item.classList.remove("item-cursos");
+        item.classList.add("item-cursosx");
+      });
+    });
+    gradeCursos.addEventListener("mouseout", function () {
+      const cursosFeitos = document.querySelectorAll(".etq5 h3");
+      cursosFeitos.forEach((item) => {
+        item.classList.add("item-cursos");
+        item.classList.remove("item-cursosx");
+      });
     });
   });
-  gradeCursos.addEventListener("mouseout", function () {
-    const cursosFeitos = document.querySelectorAll(".etq4 h3");
-    cursosFeitos.forEach((item) => {
-      item.classList.add("item-cursos");
-      item.classList.remove("item-cursosx");
-    });
-  });
-});
-
-cursosFeitos.forEach((items) => {
-  const gradeCursos = document.querySelector(".etq5");
-  gradeCursos.addEventListener("mouseover", function () {
-    const items = document.querySelectorAll(".etq5 h3");
-    items.forEach((item) => {
-      item.classList.remove("item-cursos");
-      item.classList.add("item-cursosx");
-    });
-  });
-  gradeCursos.addEventListener("mouseout", function () {
-    const cursosFeitos = document.querySelectorAll(".etq5 h3");
-    cursosFeitos.forEach((item) => {
-      item.classList.add("item-cursos");
-      item.classList.remove("item-cursosx");
-    });
-  });
-});
+}
+cursosCompletos();
 
 function initAnimacaoScroll() {
-  const sections = document.querySelectorAll('.js-scroll');
-  if(sections.length) {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
     const windowMetade = window.innerHeight * 0.6;
 
     function animaScroll() {
       sections.forEach((section) => {
         const sectionTop = section.getBoundingClientRect().top;
-        const isSectionVisible = (sectionTop - windowMetade) < 0;
-        if(isSectionVisible)
-          section.classList.add('ativo');
-        else 
-          section.classList.remove('ativo');
-      })
+        const isSectionVisible = sectionTop - windowMetade < 0;
+        if (isSectionVisible) section.classList.add("ativo");
+        else section.classList.remove("ativo");
+      });
     }
 
     animaScroll();
 
-    window.addEventListener('scroll', animaScroll);
+    window.addEventListener("scroll", animaScroll);
   }
 }
 initAnimacaoScroll();
@@ -134,23 +138,16 @@ function initScrollSuave() {
 
   function scrollToSection(event) {
     event.preventDefault();
-    const href = event.currentTarget.getAttribute('href');
+    const href = event.currentTarget.getAttribute("href");
     const section = document.querySelector(href);
     section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
+      behavior: "smooth",
+      block: "start",
     });
-
-    // forma alternativa
-    // const topo = section.offsetTop;
-    // window.scrollTo({
-    //   top: topo,
-    //   behavior: 'smooth',
-    // });
   }
 
   linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection);
+    link.addEventListener("click", scrollToSection);
   });
 }
 initScrollSuave();
