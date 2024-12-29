c9a179f848b1a12182e0a3f1f9d2792a52c5dadc
Daniel Santos <monkeydluffysan2021@gmail.com>
2024-12-28 22:43:55 -0300

    refatorando o codigo para adicionar projetos com json

---
+++ script/modules/addProjetos.js
@@ -0,0 +1,193 @@
+const cardsData = [
+  {
+    imgSrc: 'imgens/sites/OrdemServico.png',
+    title: 'Ordem de Serviço',
+    description:
+      'App web criado para suprir minha nessecidade de imprimir ordem de serviços. contem modo claro e modo escuro.',
+    siteLink: 'https://danielzamperlini.github.io/dkcell/',
+    codeLink: 'https://github.com/DanielZamperlini/OrdemServico',
+  },
+  {
+    imgSrc: 'imgens/sites/bickraft.png',
+    title: 'Bikcraft',
+    description:
+      ' Aqui utilizamos HTML, CSS e JavaScript. Nesse Projeto tambem fizemos a parte de UI e de UX.',
+    siteLink: 'https://danielzamperlini.github.io/Bikcraft/',
+    codeLink: 'https://github.com/DanielZamperlini/Bikcraft',
+  },
+  {
+    imgSrc: 'imgens/sites/picolequebradosite.png',
+    title: 'Site Picolé quebrado',
+    description:
+      ' Site com whatsapp integrado para enviar a lista de compra do carrinho direto para uma conversa com o whatsapp.',
+    siteLink: 'https://picolequebradoskateboard.netlify.app/',
+    codeLink: 'https://github.com/DanielZamperlini/lojapicolequebrado',
+  },
+  {
+    imgSrc: 'imgens/sites/surf.png',
+    title: 'Escola de Surf',
+    description:
+      'Landing Page elaborada para divulgar Uma escola de surf, com horário de atendimento e os contatos nescessários.',
+    siteLink: 'https://danielzamperlini.github.io/surfbot/',
+    codeLink: 'https://github.com/DanielZamperlini/surfbot',
+  },
+  {
+    imgSrc: 'imgens/sites/portifolioCliente.png',
+    title: 'Portifólio',
+    description:
+      'Portifolio criado para um cliente, para apresentar seu trabalho e divulgar seu talento com mais profissionalismo.',
+    siteLink: 'https://danielzamperlini.github.io/DenilsomProcop.github.io/',
+    codeLink: 'https://github.com/DanielZamperlini/DenilsomProcop.github.io',
+  },
+  {
+    imgSrc: 'imgens/sites/tecBlog.png',
+    title: 'TecBlog',
+    description:
+      'Blog de tecnologia criado em curso com finalidade de aprendizado, mas que pode ser adaptado para trabalho.',
+    siteLink: 'https://danielzamperlini.github.io/tecblog/',
+    codeLink: 'https://github.com/DanielZamperlini/tecblog',
+  },
+  {
+    imgSrc: 'imgens/sites/AnimaisFantasticos.png',
+    title: 'Animais-Fantasticos',
+    description:
+      'Site desenvolvido no curso de JavaScript completo, onde aprendemos do zero sobre a linguagem.',
+    siteLink: 'https://danielzamperlini.github.io/animais-fantasticos/',
+    codeLink: 'https://github.com/DanielZamperlini/animais-fantasticos',
+  },
+  {
+    imgSrc: 'imgens/sites/caravan.png',
+    title: 'Caravan',
+    description:
+      'Aqui temos uma Landing Page para atender o ramo de viagems, bem clean e com um design bem leve.',
+    siteLink: 'https://danielzamperlini.github.io/caravan/',
+    codeLink: 'https://github.com/DanielZamperlini/caravan',
+  },
+  {
+    imgSrc: 'imgens/sites/finas.png',
+    title: 'finans',
+    description:
+      ' Landing Page para site de finanças, criado apenas com html e css. podendo implementar outra linguagem.',
+    siteLink: 'https://danielzamperlini.github.io/finans/',
+    codeLink: 'https://github.com/DanielZamperlini/finans',
+  },
+  {
+    imgSrc: 'imgens/sites/flexBlog.png',
+    title: 'FlexBlog',
+    description:
+      'Site criado ao aprender sobre flexbox, suas funcionalidades, como usar e quando usar.',
+    siteLink: 'https://danielzamperlini.github.io/Flexblog.github.io/',
+    codeLink: 'https://github.com/DanielZamperlini/Flexblog.github.io',
+  },
+  {
+    imgSrc: 'imgens/sites/googleGlass.png',
+    title: 'GoogleGlass',
+    description:
+      ' Um dos primeiros sites que fiz no "Curso em video" do Professor Gustavo Guanabara .',
+    siteLink: 'https://danielzamperlini.github.io/projeto-glass-html5/',
+    codeLink: 'https://github.com/DanielZamperlini/projeto-glass-html5',
+  },
+  {
+    imgSrc: 'imgens/sites/hundle.png',
+    title: 'Hundle',
+    description:
+      'pagina criada para fins de estudos onde foi testado a criatividade na criação de uma Landing Pages.',
+    siteLink: '#',
+    codeLink: '#',
+  },
+  {
+    imgSrc: 'imgens/sites/leScone.png',
+    title: 'Le_scone',
+    description:
+      'Site simples de um restaurante, criado com SASS para aprendizagem com linguagens pré-processadas.',
+    siteLink: 'https://danielzamperlini.github.io/Le_scone/',
+    codeLink: 'https://github.com/DanielZamperlini/Le_scone',
+  },
+  {
+    imgSrc: 'imgens/sites/insureLandingPage.png',
+    title: 'Insure',
+    description:
+      'Landing Page<br /> para apresentar um negocio ou um produto, para conversão de vendas.',
+    siteLink: '#',
+    codeLink: '#',
+  },
+  {
+    imgSrc: 'imgens/sites/mobili.png',
+    title: 'Mobili',
+    description:
+      'Mais uma Landing Page criada para apresentar um modo diferente de se apresentar para o mundo.',
+    siteLink: '#',
+    codeLink: '#',
+  },
+  {
+    imgSrc: 'imgens/sites/museu.png',
+    title: 'Museu',
+    description:
+      ' Site de um museu onde contem informaçoes sobre eventos, datas e horarios de funcionamento.',
+    siteLink: 'https://danielzamperlini.github.io/museu/',
+    codeLink: 'https://github.com/DanielZamperlini/museu',
+  },
+  {
+    imgSrc: 'imgens/sites/nature.png',
+    title: 'Nature',
+    description:
+      'Site Criado no Curso da "Origamid" de jQuery, feito como base de aprendizagem da linguagem.',
+    siteLink: 'https://danielzamperlini.github.io/Animais_espirituais_jQuery/',
+    codeLink: 'https://github.com/DanielZamperlini/Animais_espirituais_jQuery',
+  },
+  {
+    imgSrc: 'imgens/sites/noticiasCidade.png',
+    title: 'Noticias Cidade',
+    description:
+      'Blog de noticias bem diversificado com galeria postagens, esse é um projeto para dar continuidade.',
+    siteLink: 'https://danielzamperlini.github.io/noticias-cidade/',
+    codeLink: 'https://github.com/DanielZamperlini/noticias-cidade',
+  },
+  {
+    imgSrc: 'imgens/sites/portifolio.png',
+    title: 'Portifolio',
+    description:
+      ' Este é um Portifolio todo em Inglês para uma busca de oportunidade fora do País, Criado com SASS.',
+    siteLink: 'https://danielzamperlini.github.io/portifolio/',
+    codeLink: 'https://github.com/DanielZamperlini/portifolio',
+  },
+];
+
+function createCard(card) {
+  return `
+            <div class="card">
+              <img class="card__img" src="${card.imgSrc}" alt="${card.title}">
+              <div class="card__descr-wrapper">
+                <p class="card__title">${card.title}</p>
+                <p class="card__descr">
+                  ${card.description}
+                </p>
+                <div class="card__links">
+                  <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512" class="svg">&lt;<path d="M562.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L405.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C189.5 251.2 196 330 246 380c56.5 56.5 148 56.5 204.5 0L562.8 267.7zM43.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C57 372 57 321 88.5 289.5L200.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C416.5 260.8 410 182 360 132c-56.5-56.5-148-56.5-204.5 0L43.2 244.3z"></path></svg>
+                    <a class="link" href="${card.siteLink}" target="_blank">ver site</a>
+                  </div>
+                  <div>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 496 512" class="svg">
+                      <path d="M165.9 397.4c0 2-2.3 3.6-5.2 3.6-3.3.3-5.6-1.3-5.6-3.6 0-2 2.3-3.6 5.2-3.6 3-.3 5.6 1.3 5.6 3.6zm-31.1-4.5c-.7 2 1.3 4.3 4.3 4.9 2.6 1 5.6 0 6.2-2s-1.3-4.3-4.3-5.2c-2.6-.7-5.5.3-6.2 2.3zm44.2-1.7c-2.9.7-4.9 2.6-4.6 4.9.3 2 2.9 3.3 5.9 2.6 2.9-.7 4.9-2.6 4.6-4.6-.3-1.9-3-3.2-5.9-2.9zM244.8 8C106.1 8 0 113.3 0 252c0 110.9 69.8 205.8 169.5 239.2 12.8 2.3 17.3-5.6 17.3-12.1 0-6.2-.3-40.4-.3-61.4 0 0-70 15-84.7-29.8 0 0-11.4-29.1-27.8-36.6 0 0-22.9-15.7 1.6-15.4 0 0 24.9 2 38.6 25.8 21.9 38.6 58.6 27.5 72.9 20.9 2.3-16 8.8-27.1 16-33.7-55.9-6.2-112.3-14.3-112.3-110.5 0-27.5 7.6-41.3 23.6-58.9-2.6-6.5-11.1-33.3 2.6-67.9 20.9-6.5 69 27 69 27 20-5.6 41.5-8.5 62.8-8.5s42.8 2.9 62.8 8.5c0 0 48.1-33.6 69-27 13.7 34.7 5.2 61.4 2.6 67.9 16 17.7 25.8 31.5 25.8 58.9 0 96.5-58.9 104.2-114.8 110.5 9.2 7.9 17 22.9 17 46.4 0 33.7-.3 75.4-.3 83.6 0 6.5 4.6 14.4 17.3 12.1C428.2 457.8 496 362.9 496 252 496 113.3 383.5 8 244.8 8zM97.2 352.9c-1.3 1-1 3.3.7 5.2 1.6 1.6 3.9 2.3 5.2 1 1.3-1 1-3.3-.7-5.2-1.6-1.6-3.9-2.3-5.2-1zm-10.8-8.1c-.7 1.3.3 2.9 2.3 3.9 1.6 1 3.6.7 4.3-.7.7-1.3-.3-2.9-2.3-3.9-2-.6-3.6-.3-4.3.7zm32.4 35.6c-1.6 1.3-1 4.3 1.3 6.2 2.3 2.3 5.2 2.6 6.5 1 1.3-1.3.7-4.3-1.3-6.2-2.2-2.3-5.2-2.6-6.5-1zm-11.4-14.7c-1.6 1-1.6 3.6 0 5.9 1.6 2.3 4.3 3.3 5.6 2.3 1.6-1.3 1.6-3.9 0-6.2-1.4-2.3-4-3.3-5.6-2z"></path>
+                    </svg>
+                    <a class="link" href="${card.codeLink}" target="_blank">Code</a>
+                  </div>
+                </div>
+              </div>
+            </div>
+    
+  `;
+}
+
+const isMainPage = window.location.pathname === '/index.html'; // Verifica se está na página principal
+
+const container = document.querySelector('.cards-container');
+
+// Se for a página principal, mostra apenas os 4 primeiros
+const cardsToDisplay = isMainPage ? cardsData.slice(0, 4) : cardsData;
+
+cardsToDisplay.forEach((card) => {
+  const cardHTML = createCard(card);
+  container.innerHTML += cardHTML;
+});
--- script/script.js
+++ script/script.js
@@ -1,9 +1,9 @@
-import navbarMenu from "./modules/toogle.js";
-import cursosCompletos from "./modules/infoCursos.js";
-import removeStilo from "./modules/removeEstilo.js";
-import initAnimacaoScroll from "./modules/animacaoScroll.js";
-import initScrollSuave from "./modules/scrollSuave.js";
-import initModal from "./modules/modal.js";
+import navbarMenu from './modules/toogle.js';
+import cursosCompletos from './modules/infoCursos.js';
+import removeStilo from './modules/removeEstilo.js';
+import initAnimacaoScroll from './modules/animacaoScroll.js';
+import initScrollSuave from './modules/scrollSuave.js';
+import initModal from './modules/modal.js';
 
 navbarMenu();
 cursosCompletos();
@@ -11,4 +11,3 @@ removeStilo();
 initAnimacaoScroll();
 initScrollSuave();
 initModal();
-
